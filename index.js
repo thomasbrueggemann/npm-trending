fce9775f44f0370884ce00c43db3dedc4f8c793b
Thomas Brüggemann <mail@thomasbrueggemann.com>
2017-04-24 21:49:48 +0200

    rewrite database connection code

--- index.js
+++ index.js
@@ -5,207 +5,206 @@ const moment = require("moment");
 const async = require("async");
 
 const mongodbUrl = "mongodb://localhost:27017/npm";
+MongoClient.connect(mongodbUrl, (err, db) => {
+	var downloadsCol = db.collection("downloads");
+	var packagesCol = db.collection("packages");
 
-// STORE RECENTLY UPDATED
-function storeRecentlyUpdated() {
-    console.log(new Date(), "storeRecentlyUpdated()");
-    var packages = [];
+	// STORE RECENTLY UPDATED
+	function storeRecentlyUpdated() {
+		console.log(new Date(), "storeRecentlyUpdated()");
+		var packages = [];
 
-    // fetch recently updated
-    request("https://www.npmjs.com/browse/updated", (error, response, body) => {
-        // the request has a valid response
-        if (response && response.statusCode === 200 && response.body) {
-            // parse html with cheerio
-            var $ = cheerio.load(response.body);
+		// fetch recently updated
+		request(
+			"https://www.npmjs.com/browse/updated",
+			(error, response, body) => {
+				// the request has a valid response
+				if (response && response.statusCode === 200 && response.body) {
+					// parse html with cheerio
+					var $ = cheerio.load(response.body);
 
-            $("a[class='name']").each((i, a) => {
-                var href = $(a).attr("href");
-                if (href.indexOf("/package/") === 0) {
-                    packages.push(href.replace("/package/", ""));
-                }
-            });
-        }
+					$("a[class='name']").each((i, a) => {
+						var href = $(a).attr("href");
+						if (href.indexOf("/package/") === 0) {
+							packages.push(href.replace("/package/", ""));
+						}
+					});
+				}
 
-        // store in mongodb
-        // open mongodb connection
-        MongoClient.connect(mongodbUrl, (err, db) => {
-            var collection = db.collection("packages");
+				// store in mongodb
 
-            // insert each package
-            async.each(
-                packages,
-                (p, done) => {
-                    collection.insert(
-                        {
-                            _id: p,
-                            upt: new Date()
-                        },
-                        (err, result) => {
-                            return done();
-                        }
-                    );
-                },
-                err => {
-                    db.close();
-                }
-            );
-        });
-    });
-}
+				// insert each package
+				async.each(
+					packages,
+					(p, done) => {
+						packagesCol.insert(
+							{
+								_id: p,
+								upt: new Date()
+							},
+							(err, result) => {
+								return done();
+							}
+						);
+					},
+					err => {}
+				);
+			}
+		);
+	}
 
-// download newly updated packages every minute
-setInterval(storeRecentlyUpdated, 60000);
-storeRecentlyUpdated();
+	// download newly updated packages every minute
+	setInterval(storeRecentlyUpdated, 60000);
+	storeRecentlyUpdated();
 
-// CHECK NEXT PACKAGE
-function checkNextPackage(callback) {
-    MongoClient.connect(mongodbUrl, (err, db) => {
-        var downloadsCol = db.collection("downloads");
-        var packagesCol = db.collection("packages");
+	// CHECK NEXT PACKAGE
+	function checkNextPackage(callback) {
+		// find some packages
+		packagesCol
+			.find({
+				upt: {
+					$lt: moment().utc().subtract(1, "h").toDate()
+				}
+			})
+			.sort(["upt", 1])
+			.limit(5)
+			.toArray(function(err, packages) {
+				if (err) return callback(err);
 
-        // find some packages
-        packagesCol
-            .find({
-                upt: {
-                    $lt: moment().utc().subtract(1, "h").toDate()
-                }
-            })
-            .sort(["upt", 1])
-            .limit(5)
-            .toArray(function(err, packages) {
-                if (err) return callback(err);
+				async.each(
+					packages,
+					(pkg, ready) => {
+						// download the npm download counts for today
+						request(
+							"https://www.npmjs.com/package/" + pkg._id,
+							(error, response, body) => {
+								// the request has a valid response
+								if (
+									response &&
+									response.statusCode === 200 &&
+									response.body
+								) {
+									// parse html with cheerio
+									var $ = cheerio.load(response.body);
 
-                async.each(
-                    packages,
-                    (pkg, ready) => {
-                        // download the npm download counts for today
-                        request(
-                            "https://www.npmjs.com/package/" + pkg._id,
-                            (error, response, body) => {
-                                // the request has a valid response
-                                if (
-                                    response &&
-                                    response.statusCode === 200 &&
-                                    response.body
-                                ) {
-                                    // parse html with cheerio
-                                    var $ = cheerio.load(response.body);
+									var count = $(".daily-downloads")
+										.text()
+										.replace(/[^\d]/g, "");
 
-                                    var count = $(".daily-downloads")
-                                        .text()
-                                        .replace(/[^\d]/g, "");
+									// store download count
+									downloadsCol.updateOne(
+										{
+											_id: {
+												pkg: pkg._id,
+												date: moment()
+													.startOf("day")
+													.toDate()
+											}
+										},
+										{
+											$set: {
+												dl: parseInt(count)
+											}
+										},
+										{
+											upsert: true
+										}
+									);
 
-                                    // store download count
-                                    downloadsCol.updateOne(
-                                        {
-                                            _id: {
-                                                pkg: pkg._id,
-                                                date: moment()
-                                                    .startOf("day")
-                                                    .toDate()
-                                            }
-                                        },
-                                        {
-                                            $set: {
-                                                dl: parseInt(count)
-                                            }
-                                        },
-                                        {
-                                            upsert: true
-                                        }
-                                    );
+									// update the update date on the package
+									packagesCol.updateOne(
+										{
+											_id: pkg._id
+										},
+										{
+											$set: {
+												upt: new Date()
+											}
+										}
+									);
 
-                                    // update the update date on the package
-                                    packagesCol.updateOne(
-                                        {
-                                            _id: pkg._id
-                                        },
-                                        {
-                                            $set: {
-                                                upt: new Date()
-                                            }
-                                        }
-                                    );
+									// download package config
+									request(
+										"https://unpkg.com/" +
+											pkg._id +
+											"/package.json",
+										(error, response, body) => {
+											var packagejson = null;
+											if (
+												!error &&
+												response.statusCode === 200
+											) {
+												packagejson = JSON.parse(body);
+											}
 
-                                    // download package config
-                                    request(
-                                        "https://unpkg.com/" +
-                                            pkg._id +
-                                            "/package.json",
-                                        (error, response, body) => {
-                                            var packagejson = null;
-                                            if (
-                                                !error &&
-                                                response.statusCode === 200
-                                            ) {
-                                                packagejson = JSON.parse(body);
-                                            }
+											if (packagejson !== null) {
+												packagesCol.updateOne(
+													{
+														_id: pkg._id
+													},
+													{
+														$set: {
+															desc: packagejson.description,
+															ver: packagejson.version,
+															keys: packagejson.keywords
+														},
+														$unset: {
+															pkg: true
+														}
+													},
+													(err, result) => {
+														// store dependencies
+														if (
+															"dependencies" in
+															packagejson
+														) {
+															for (var d in Object.keys(
+																packagejson.dependencies
+															)) {
+																packagesCol.insert(
+																	{
+																		_id: Object.keys(
+																			packagejson.dependencies
+																		)[d],
+																		upt: new Date()
+																	},
+																	(
+																		err,
+																		result
+																	) => {}
+																);
+															}
+														}
+													}
+												);
+											}
+										}
+									);
 
-                                            if (packagejson !== null) {
-                                                packagesCol.updateOne(
-                                                    {
-                                                        _id: pkg._id
-                                                    },
-                                                    {
-                                                        $set: {
-                                                            desc: packagejson.description,
-                                                            ver: packagejson.version,
-                                                            keys: packagejson.keywords
-                                                        },
-                                                        $unset: {
-                                                            pkg: true
-                                                        }
-                                                    },
-                                                    (err, result) => {
-                                                        // store dependencies
-                                                        if (
-                                                            "dependencies" in
-                                                            packagejson
-                                                        ) {
-                                                            for (var d in Object.keys(
-                                                                packagejson.dependencies
-                                                            )) {
-                                                                packagesCol.insert(
-                                                                    {
-                                                                        _id: Object.keys(
-                                                                            packagejson.dependencies
-                                                                        )[d],
-                                                                        upt: new Date()
-                                                                    },
-                                                                    (err, result) => {}
-                                                                );
-                                                            }
-                                                        }
-                                                    }
-                                                );
-                                            }
-                                        }
-                                    );
+									console.log(
+										new Date(),
+										"checkNextPackage(" + pkg._id + ") -> ",
+										parseInt(count)
+									);
+								} else if (response.statusCode === 404) {
+									// remove the package, because it apparently does not exist anymore
+									packagesCol.deleteOne({
+										_id: pkg._id
+									});
+								}
 
-                                    console.log(
-                                        new Date(),
-                                        "checkNextPackage(" + pkg._id + ") -> ",
-                                        parseInt(count)
-                                    );
-                                } else if (response.statusCode === 404) {
-                                    // remove the package, because it apparently does not exist anymore
-                                    packagesCol.deleteOne({
-                                        _id: pkg._id
-                                    });
-                                }
+								return ready();
+							}
+						);
+					},
+					err => {
+						return callback();
+					}
+				);
+			});
+	}
 
-                                return ready();
-                            }
-                        );
-                    },
-                    err => {
-                        return callback();
-                    }
-                );
-            });
-    });
-}
-
-async.forever(checkNextPackage, err => {
-    console.error(err);
+	async.forever(checkNextPackage, err => {
+		console.error(err);
+	});
 });
